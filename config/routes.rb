b513e81f078ab41e6572902ada5787fbe2ad2575
gmrind <murtazarind.11@gmail.com>
2015-12-15 03:46:24 +0500

    one more time going to makes this one...

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+  devise_for :users,
+    :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :sign_up => 'admin/new/signup'}
+
+  root                  'pages#home'
+  get   'about'    =>   'pages#about'
+  get   'contact'  =>   'pages#contact'
+  get   'news'     =>   'pages#news'
+  get   'help'     =>   'pages#help'
+
+  resources :users
+  resources :courses do
+    resources :coursesemesters do
+        end
+  end
+
+end
